0fb6e23d5e167310eaeeb1650c987a89f719d3e0
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-10-18 09:06:27 +0200

    koniec lekcji

--- Programowanie-1E/DecisionInstruction/DecisionInstruction.cpp
+++ Programowanie-1E/DecisionInstruction/DecisionInstruction.cpp
@@ -5,23 +5,28 @@ Napisz funkcjê, któa wczyta dwie liczby i wykona dzielenie na nich
 */
 void task1()
 {
-    int firstNumber, secondNumber;
+	int firstNumber, secondNumber;
 
-    std::cout << "Podaj pierwsza liczbe:\n";
-    std::cin >> firstNumber;
+	std::cout << "Podaj pierwsza liczbe:\n";
+	std::cin >> firstNumber;
 
-    std::cout << "Podaj druga liczbe (nie podawaj zera):\n";
-    std::cin >> secondNumber;
+	std::cout << "Podaj druga liczbe (nie podawaj zera):\n";
+	std::cin >> secondNumber;
 
-    //iloraz
-    int quotient;
+	//iloraz
+	int quotient = 0;
 
-    quotient = firstNumber / secondNumber;
+	if (secondNumber != 0)
+		quotient = firstNumber / secondNumber;
 
-    std::cout << "Iloraz: " << quotient << "\n";
+	if (secondNumber != 0)
+		std::cout << "Iloraz: " << quotient << "\n";
+
+	if (secondNumber == 0)
+		std::cout << "Dzielenie przez zero!!!!\n";
 }
 
 int main()
 {
-    task1();
+	task1();
 }
