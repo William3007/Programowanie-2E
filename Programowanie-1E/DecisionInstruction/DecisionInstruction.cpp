7e618083a6387a076b04664cb3f15084c20f10d0
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-10-20 15:14:18 +0200

    koniec lekcji

--- Programowanie-1E/DecisionInstruction/DecisionInstruction.cpp
+++ Programowanie-1E/DecisionInstruction/DecisionInstruction.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 /*
+DRY - don't repeat yourself - nie powtarzaj siê
+*/
+
+/*
 Napisz funkcjê, któa wczyta dwie liczby i wykona dzielenie na nich
 */
 void task1()
@@ -16,14 +20,25 @@ void task1()
 	//iloraz
 	int quotient = 0;
 
-	if (secondNumber != 0)
+	/*if (secondNumber != 0)
 		quotient = firstNumber / secondNumber;
 
 	if (secondNumber != 0)
 		std::cout << "Iloraz: " << quotient << "\n";
 
 	if (secondNumber == 0)
+		std::cout << "Dzielenie przez zero!!!!\n";*/
+
+	if (secondNumber != 0)
+	{
+		quotient = firstNumber / secondNumber;
+		std::cout << "Iloraz: " << quotient << "\n";
+	}
+
+	if (secondNumber == 0)
+	{
 		std::cout << "Dzielenie przez zero!!!!\n";
+	}
 }
 
 int main()
