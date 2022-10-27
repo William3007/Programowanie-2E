fd866d63075e5c411fb0dc3abbcd0f163c07f82c
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-10-27 15:11:02 +0200

    koniec lekcji

--- Programowanie-1E/DecisionInstruction/DecisionInstruction.cpp
+++ Programowanie-1E/DecisionInstruction/DecisionInstruction.cpp
@@ -5,6 +5,26 @@ DRY - don't repeat yourself - nie powtarzaj siê
 */
 
 /*
+Operatory porównania:
+
+* równoœci				==
+* ró¿noœci				!=
+* wiêkszoœci			>
+* mniejszoœci			<
+* wiêksze lub równe		>=
+* mniejsze lub równe	<=
+
+argument1 >= argument2
+
+Wynikiem dzia³ania tych operatorów 
+jest wartoœæ logiczna true/false typu bool
+
+bool zmienna = true;
+bool zmienna2 = 5 > 74;
+
+*/
+
+/*
 Napisz funkcjê, któa wczyta dwie liczby i wykona dzielenie na nich
 */
 void task1()
@@ -53,11 +73,30 @@ void task2()
 	std::cout << "Podaj liczbe do sprawdzenia:\n";
 	std::cin >> numberToCheck;
 
+	if (numberToCheck > 0)
+		std::cout << "Liczba wiêksza od zera\n";
+	else
+		std::cout << "Liczba równa lub mniejsza od zera\n";
+}
 
+/*
+Napisz funkcjê, która wczyta liczbê i wyœwietli informacjê
+czy jest ona w zakresie <10, 25>.
+*/
+void task3()
+{
+	int numberToCheck;
+
+	std::cout << "Podaj liczbe do sprawdzenia:\n";
+	std::cin >> numberToCheck;
+
+	std::cout << "Liczba jest w zakresie\n";
+	std::cout << "Liczba nie jest w zakresie\n";
 }
 
 int main()
 {
 	//task1();
-	task2();
+	//task2();
+	task3();
 }
