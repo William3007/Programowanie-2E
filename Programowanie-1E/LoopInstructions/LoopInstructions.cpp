803dcb598ae200473e52dba5af7a8690898e6205
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-11-23 09:53:35 +0100

    koniec lekcji

--- Programowanie-1E/LoopInstructions/LoopInstructions.cpp
+++ Programowanie-1E/LoopInstructions/LoopInstructions.cpp
@@ -16,12 +16,58 @@ void task1()
     } while (number <= 0);
 
     std::cout << "Poda³eœ liczbê " << number << "\n";
-
 }
+
+/*
+Zadanie 2
+Napisz funkcjê, która wyœwietli wszystkie liczby ca³kowite z przedzia³u <1;10>
+*/
+
+void task2()
+{
+    std::cout << "1, 2, 3, 4, 5, 6, 7, 8, 9, 10 \n";
+
+    int currentNumber = 1;
+
+    do
+    {
+        std::cout << currentNumber << ", ";
+        //currentNumber = currentNumber + 1;
+        //currentNumber += 1;
+        currentNumber++;
+    } while (currentNumber != 11); // (currentNumber < 11)
+}
+
+/*
+Zadanie 3
+Napisz funkcjê, która wyœwietli wszystkie liczby ca³kowite z przedzia³u pobranego od u¿ytkownika
+*/
+
+void task3()
+{
+    int lowRange, highRange;
+    std::cout << "Podaj zakres dolny\n";
+    std::cin >> lowRange;
+    std::cout << "Podaj górny zakres\n";
+    std::cin >> highRange;
+
+    int currentNumber = 1;
+
+    do
+    {
+        std::cout << currentNumber << ", ";
+        //currentNumber = currentNumber + 1;
+        //currentNumber += 1;
+        currentNumber++;
+    } while (currentNumber != 11); // (currentNumber < 11)
+}
+
 
 int main()
 {
     setlocale(LC_CTYPE, "polish");
 
-    task1();
+    //task1();
+    //task2();
+    task3();
 }
