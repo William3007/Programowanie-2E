2338604953d111041b1552e6c4a18a8a87c1d386
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-03 15:06:57 +0200

    nlkbnhuibnuino

--- Programowanie-1E/TableConsoleApp/TableConsoleApp.cpp
+++ Programowanie-1E/TableConsoleApp/TableConsoleApp.cpp
@@ -1,174 +1,110 @@
 ﻿#include <iostream>
-
-void task1()
+using namespace std;
+/*
+Napisz program/funkcję która dla tablicy z liczbami pseudolosowymi policzy
+średnią arytmetyczną tych liczb.
+*/
+void zad1()
 {
-	int a;
-	int tab[50];//0,1,2
+	const int SizeOfTable = 3;
+	int tab[SizeOfTable];
+	float avarageOfNumber;
+	float sumeOfNumbers = 0;
 
-	a = 5;
-	tab[0] = 5;
-	tab[1] = 3;
-	tab[2] = 87;
-
-	a = a * tab[1];
-
-
-	int x, y, z;
-	std::cin >> x;
-	std::cin >> y;
-	std::cin >> z;
-
-	std::cin >> tab[0];
-	std::cin >> tab[1];
-	std::cin >> tab[2];
-
-	for (int i = 0; i < 3; i++)
+	srand(time(NULL));
+	for (int i = 0; i < SizeOfTable; i++)
 	{
-		std::cin >> tab[i];
+		tab[i] = rand() % 10;
+		sumeOfNumbers = sumeOfNumbers + tab[i];
 	}
 
+	avarageOfNumber = sumeOfNumbers / SizeOfTable;
+
+	for (int i = 0; i < SizeOfTable; i++)
+	{
+		cout << tab[i] << ", ";
+	}
+	cout << endl << "srednia wynosi = " << avarageOfNumber;
+}
+
+
+/*
+* Napisz funkcję, która dla kolekcji danych liczbowych policzy ile jest liczb większych od średniej arytmetycznej.
+ */
+void zad2()
+{
+	const int SizeOfTable = 3;
+	int tab[SizeOfTable];
+	float avarageOfNumber;
+	float sumeOfNumbers = 0;
+
+	srand(time(NULL));
+	for (int i = 0; i < SizeOfTable; i++)
+	{
+		tab[i] = rand() % 10;
+		sumeOfNumbers = sumeOfNumbers + tab[i];
+		cout << tab[i] << ", ";
+	}
+	avarageOfNumber = sumeOfNumbers / SizeOfTable;
+
+	int NumbersBiggerThenAvarage = 0;
+
+	for (int i = 0; i < SizeOfTable; i++)
+	{
+		if (tab[i] > avarageOfNumber)
+		{
+			NumbersBiggerThenAvarage++;
+		}
+	}
+
+	cout << endl << "Liczb wiekszych od sredniej rownej: " << avarageOfNumber << " Jest =  " << NumbersBiggerThenAvarage << endl;
 }
 
 /*
-Wyświetl największą liczbę ze zbioru
+* Napisz funkcje, która dla kolekcji danych liczbowych przeniesie te liczby do innej kolekcji w odwrotnej kolejności.
 */
-void task2()
+void zad3()
 {
-	const int size = 10;
-
-	int tabOfNumbers[size];
+	const int SizeOfTable = 3;
+	int firsttab[SizeOfTable];
+	int secondTab[SizeOfTable];
 
 	srand(time(NULL));
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < SizeOfTable; i++)
 	{
-		tabOfNumbers[i] = rand() % 10 + 1;
+		firsttab[i] = rand() % 10;
+		cout << firsttab[i] << ", ";
+	}
+	cout << endl;
+
+	for (int i = 0, j = SizeOfTable - 1; i < SizeOfTable; i++, j--)
+	{
+		secondTab[j] = firsttab[i];
 	}
 
-	std::cout << "Liczby w tablicy:\n";
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < SizeOfTable; i++)
 	{
-		std::cout << tabOfNumbers[i] << "\n";
+		cout << secondTab[i] << ", ";
 	}
-
-	//szukanie największej
-	int max = tabOfNumbers[0];
-	for (int i = 1; i < size; i++)
-	{
-		if (max < tabOfNumbers[i])
-			max = tabOfNumbers[i];
-	}
-	std::cout << "Największa liczba w zbiorze to " << max << "\n";
-
-	//szukanie najmniejszej
-	int min = tabOfNumbers[0];
-	for (int i = 1; i < size; i++)
-	{
-		if (min > tabOfNumbers[i])
-			min = tabOfNumbers[i];
-	}
-
-	std::cout << "Najmniejsza liczba w zbiorze to " << min << "\n";
-
-	float sum = 0;
-	for (int i = 0; i < size; i++)
-		sum = sum + tabOfNumbers[i];
-
-	std::cout << "Suma liczb: " << sum << "\n";
-	std::cout << "Średnia liczb:" << sum / size << "\n";
-
-	int count = 0;
-	float average = sum / size;
-	for (int i = 0; i < size; i++)
-		if (tabOfNumbers[i] > average)
-			count++;
-	std::cout << "Ilość liczb większa od średniej " << count << "\n";
 }
 
-/*Zaimplementuj algorytm sortowania przez wybór*/
-void task3()
-{
-	const int size = 10;
+/*
+* Napisz funkcje, która dla kolekcji danych liczbowych obliczy częstotliwość występowania danej liczby.
+*/
 
-	int tabOfNumbers[size];
+/*
 
-	srand(time(NULL));
-	for (int i = 0; i < size; i++)
-	{
-		tabOfNumbers[i] = rand() % 10 + 1;
-	}
+* Napisz funkcję, która dla kolekcji danych liczbowych znajdzie najdłuższy rosnący podciąg.
+* Napisz funkcję, która dla kolekcji danych liczbowych przeniesie te liczby do osobnych kolekcji liczb parzystych i nieparzystych.
 
-	std::cout << "Liczby w tablicy:\n";
-	for (int i = 0; i < size; i++)
-	{
-		std::cout << tabOfNumbers[i] << ", ";
-	}
-	std::cout << "\n";
 
-	for (int i = 0; i < size; i++)
-	{
-		int min = i;
-		for (int j = i + 1; j < size; j++)
-		{
-			if (tabOfNumbers[min] > tabOfNumbers[j])
-				min = j;
-		}
-		int tmp = tabOfNumbers[i];
-		tabOfNumbers[i] = tabOfNumbers[min];
-		tabOfNumbers[min] = tmp;
-	}
-
-	std::cout << "Liczby w tablicy po posortowaniu:\n";
-	for (int i = 0; i < size; i++)
-	{
-		std::cout << tabOfNumbers[i] << ", ";
-	}
-	std::cout << "\n";
-
-}
-
-/*Zaimplementuj algorytm sortowania bąbelkowego*/
-void task4()
-{
-	const int size = 10;
-
-	int tabOfNumbers[size];
-
-	srand(time(NULL));
-	for (int i = 0; i < size; i++)
-	{
-		tabOfNumbers[i] = rand() % 10 + 1;
-	}
-
-	std::cout << "Liczby w tablicy:\n";
-	for (int i = 0; i < size; i++)
-	{
-		std::cout << tabOfNumbers[i] << ", ";
-	}
-	std::cout << "\n";
-
-	for (int i = 0; i < size - 1; i++)
-	{
-		for (int j = 0; j < size - 1; j++)
-		{
-			if (tabOfNumbers[j] > tabOfNumbers[j + 1])
-			{
-				int tmp = tabOfNumbers[j];
-				tabOfNumbers[j] = tabOfNumbers[j + 1];
-				tabOfNumbers[j + 1] = tmp;
-			}
-		}
-	}
-
-	std::cout << "Liczby w tablicy po posortowaniu:\n";
-	for (int i = 0; i < size; i++)
-	{
-		std::cout << tabOfNumbers[i] << ", ";
-	}
-	std::cout << "\n";
-
-}
+tablicaLiczb[i] = rand() % (zakresGorny - zakresDolny + 1) + zakresDolny;
+*/
 
 int main()
 {
-	task4();
+	//zad1();
+	//zad2();
+	//zad3();
+
 }
